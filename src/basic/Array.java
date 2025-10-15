36551e8efcf4bffca5ae2dc857ab00df91a80603
Eric SHIM <hyunwoo8504@gmail.com>
2025-10-15 16:05:26 +0900

    Feat: 2차원 배열 생성 및 출력

--- src/basic/Array.java
+++ src/basic/Array.java
@@ -1,19 +1,39 @@
 package basic;
 
+import java.util.Arrays;
+
 public class Array {
 
     public static void main(String[] args) {
 
-        int[] index = new int[11];
+        int[][] arr2d = new int[4][];
 
-        int[] arr = {1,1,3,3,5,6,7,8,4,10};
-        for (int num : arr) {
-            index[num]++;
+        arr2d[0] = new int[] {1};
+        arr2d[1] = new int[] {2, 3};
+        arr2d[2] = new int[] {4, 5, 6};
+        arr2d[3] = new int[] {7, 8, 9, 10};
+
+        for (int i = 0; i < arr2d.length; i++) {
+            for (int j = 0; j < arr2d[i].length; j++) {
+                System.out.print(arr2d[i][j] + " ");
+            }
+            System.out.println();
         }
 
-        for (int i = 1; i < index.length; i++) {
-            System.out.println(i + "의 출현횟수 " + index[i] + "회");
-        }
+//        System.out.println(Arrays.toString(arr2d));
+
+        /**
+         * int[] index = new int[11];
+         *
+         *         int[] arr = {1,1,3,3,5,6,7,8,4,10};
+         *         for (int num : arr) {
+         *             index[num]++;
+         *         }
+         *
+         *         for (int i = 1; i < index.length; i++) {
+         *             System.out.println(i + "의 출현횟수 " + index[i] + "회");
+         *         }
+         */
 
         /**
          *  int[] arr = {1,2,3,4,5};
