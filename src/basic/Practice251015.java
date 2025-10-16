fbe87a6f00181291c6508829a077a07ab546c8cc
Eric SHIM <hyunwoo8504@gmail.com>
2025-10-16 09:03:49 +0900

    Refactor: 251015 과제 메서드화

--- src/basic/Practice251015.java
+++ src/basic/Practice251015.java
@@ -6,6 +6,19 @@ public class Practice251015 {
 
     public static void main(String[] args) {
 
+        System.out.println("문제풀이 시작");
+        p1();
+        p2();
+        p3();
+        p4();
+        p5();
+        p6();
+        p7();
+        p8();
+        System.out.println("문제풀이 끝");
+    }
+
+    private static void p1() {
         // 1
         System.out.println("#1");
         for (int i = 1; i <= 100; i++) {
@@ -14,7 +27,9 @@ public class Practice251015 {
             }
         }
         System.out.println();
+    }
 
+    private static void p2() {
         // 2
         System.out.println("#2");
         int sum2 = 0;
@@ -24,7 +39,9 @@ public class Practice251015 {
             }
         }
         System.out.println(sum2);
+    }
 
+    private static void p3() {
         // 3
         System.out.println("#3");
         int count3 = 0;
@@ -34,7 +51,9 @@ public class Practice251015 {
             }
         }
         System.out.println(count3);
+    }
 
+    private static void p4() {
         // 4
         System.out.println("#4");
         int count4 = 1;
@@ -44,7 +63,9 @@ public class Practice251015 {
             num4 += 6;
         }
         System.out.println(num4);
+    }
 
+    private static void p5() {
         // 5
         System.out.println("#5");
         BigInteger num5 = BigInteger.valueOf(2);
@@ -56,7 +77,9 @@ public class Practice251015 {
             count5++;
         }
         System.out.println(num5);
+    }
 
+    private static void p6() {
         // 6
         System.out.println("#6");
         BigInteger[] fibonacci = new BigInteger[50];
@@ -69,7 +92,9 @@ public class Practice251015 {
         for (BigInteger fibo : fibonacci) {
             System.out.println(fibo);
         }
+    }
 
+    private static void p7() {
         // 7
         System.out.println("#7");
         BigInteger sum7 = BigInteger.ZERO;
@@ -81,7 +106,9 @@ public class Practice251015 {
             sum7 = sum7.add(temp);
         }
         System.out.println(sum7);
+    }
 
+    private static void p8() {
         // 8
         System.out.println("#8");
         BigInteger sum8 = BigInteger.ZERO;
@@ -98,8 +125,6 @@ public class Practice251015 {
             }
         }
         System.out.println(sum8);
-
     }
-
 
 }
